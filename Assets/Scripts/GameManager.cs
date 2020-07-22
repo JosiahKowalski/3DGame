4c93c116e2fb96bd634ba13cab7a5b97417695b4
Josiah <Josiah.kowalski724@gmail.com>
2020-07-22 03:33:34 -0400

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _gameHasEnded;
+
+    public float collisionDelay = 1f;
+    public float fallingDelay = 1f;
+
+    [FormerlySerializedAs("completeLevelUI")] public GameObject completeLevelUi;
+
+    public void CompleteLevel()
+    {
+        completeLevelUi.SetActive(true);
+    }
+    public void EndGame(string fall)
+    {
+        if (_gameHasEnded) return;
+        Debug.Log("game over");
+        _gameHasEnded = true;
+        Invoke(nameof(Restart), fallingDelay);
+
+    }
+    public void EndGame()
+    {
+        if (_gameHasEnded == false)
+        {
+            Debug.Log("game over");
+            _gameHasEnded = true;
+            Invoke(nameof(Restart), collisionDelay);
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // variables and such
+    public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float sidewaysForce = 100f;
+    
+
+    // FixedUpdate is called once per frame (using Fixed because physics are involved)
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+        //if (Input.GetKey("w"))
+        //{
+        //    rb.AddForce(0, 0, 500 * Time.deltaTime);
+        //}
+        //if (Input.GetKey("s"))
+        //{
+        //    rb.AddForce(0, 0, -500 * Time.deltaTime);
+        //}
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < .9f)
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            FindObjectOfType<GameManager>().EndGame("fall");
+        }
+    }
+}
